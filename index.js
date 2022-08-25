f4229e9275e0d7d5e192499060c3f95a5470e67e
Chen <liangchen_amelia@163.com>
2022-08-26 00:54:43 +0100

    second-commit

--- index.js
+++ index.js
@@ -6,59 +6,66 @@
 
     
 
-    //create board
+//create board
+//First create a 10*10 board with 20 bombs in it
+//Then give each element a unique value like bomb or no bomb(empty)
+//Then shuffle the bomb squares and empty squares together randomly when refresh the page
     const createBoard = (i) => {
 
-        //create bomb and empty squares and shufffle them in random everytime refresh the page
+//create bomb and empty squares and shufffle them in random everytime refresh the page
         let amountOfBombs = 20;
+        let totalSuqares = 100;
 
         /**Method of fill an array
          * The fill() method fills specified elements in an array with a value.
-         * Syntax: fill(value, start, end)
-         * const arr = Array(arrayName).fill(filler)
+         * Syntax: fill(value, start, end)  start: default-0  end: default-arr.length
          */
         const arrayOfBombs = Array(amountOfBombs).fill('bomb');
-        const arrayOfEmpty = Array(100 - amountOfBombs).fill('empty');
+        const arrayOfEmpty = Array(totalSuqares - amountOfBombs).fill('empty');
         console.log(arrayOfBombs);
         console.log(arrayOfEmpty);
 
         //join the bombs array and empty array together
+        //Join two arrays together: newArray = arr1.concat.arr2;
+        //                          newArray = arr1.concat.(arr2, arr3,.., arrx);
         const totalArray = arrayOfEmpty.concat(arrayOfBombs);
         console.log(totalArray);
 
         /*creat a new array to shuffle the bomb and empty squares randomly
-          the method to put elements in array sort randomly is using funcion:
-          function myFunction() {
-            points.sort(function(){return 0.5 - Math.random()});
+          Sorting an array in random order should use this funcion:
+          const newArray = () => {
+            originalArray.sort(() => 0.5 - Math.random());
+          }
          link: https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_sort_random
         */
         const shuffledArray = totalArray.sort(() => 0.5 - Math.random());
         console.log(shuffledArray);
 
 
-
-
-
-
-
         for(let i = 0; i < 100; i++) {
             //create a div and unique id for each element
+            //and pass through the square to grid
             const square = document.createElement('div');
             square.setAttribute('id', i)
 
-            //pass through the square as a parameter
             grid.appendChild(square)
+
+
+            //add class to each element in shuffled array and make sure we can add impage or text in the square
+            square.classList.add(shuffledArray[i]);
 
             //create new array of squares for bombs and push it to the square
             squares.push(square);
 
+
         }
+
+       
 
     
 
 
     }
-
     createBoard()
 
 
